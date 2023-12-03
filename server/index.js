cf156979b84ba60ef3a263c9b94e9795a885ccf8
Shiro Tang <jxt598@case.edu>
2023-12-03 16:21:42 -0500

    Added static folder and prep SPA

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/public/'))
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port${port}`));
